0ea405037c5115071d59fbe011771b60fd84d5c5
dxArtur <daniel.arturjsb@gmail.com>
2024-02-19 23:20:09 -0300

    update prisma schema

---
+++ prisma/migrations/20240217014559_insert_field_password_in_user_model/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Added the required column `password` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "password" TEXT NOT NULL,
+ALTER COLUMN "name" DROP NOT NULL;
