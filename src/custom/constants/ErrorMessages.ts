0018080d3d6c42a932afead10c04d9b1e5184edc
dxArtur <daniel.arturjsb@gmail.com>
2024-02-27 09:00:28 -0300

    create custom directory with constants

---
+++ src/custom/constants/ErrorMessages.ts
@@ -0,0 +1,8 @@
+const ERROR_MESSAGES = {
+	BAD_AUTH : 'Usuário e/ou senha inválidos',
+    USER_ALREADY_EXIST: 'Usuário já existente',
+	USER_NOT_FOUND: 'Usuário não encontrado',
+	INTERNAL_ERROR_SERVER : 'ERRO INTERNO DO SERVIDOR'
+}
+
+module.exports = {ERROR_MESSAGES}
