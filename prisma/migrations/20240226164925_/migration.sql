3e2a636d51d958a40d61b675161f90f43923fec1
dxArtur <daniel.arturjsb@gmail.com>
2024-02-27 08:56:51 -0300

    create user schema

---
+++ prisma/migrations/20240226164925_/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "nick" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "books" TEXT[],
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_nick_key" ON "users"("nick");
