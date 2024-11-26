71713ebab06951b3db9c48362b218119c2b3979f
D. Artur <daniel.arturjsb@gmail.com>
2024-11-26 11:56:50 -0300

    database

--- src/modules/users/getById/getUserByIdUseCase.ts
+++ src/modules/users/getById/getUserByIdUseCase.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client"
 import { RepositoryClient } from "../../../database/prismaClient" 
 import { AppError } from "../../../errors/AppErrors"
-import { UserDto } from "../../../dto/UserDto"
+import { sanitizedOutputSignin, UserDto } from "../../../dto/UserDto"
 import ErrorMessages from "../../../custom/constants/ErrorMessages"
 import StatusCode from "../../../custom/constants/StatusCode"
 
@@ -21,18 +21,43 @@ export class GetUserByIdUseCase{
         return GetUserByIdUseCase.instance
     }
 
-    async execute(userId: string): Promise<UserDto>{
+    async execute(userId: string): Promise<sanitizedOutputSignin>{
         try {
             const user = await this.repository.user.findUniqueOrThrow({
                 where: {
                     id: userId
+                },
+                select: {
+                    id:true,
+                    name:true,
+                    nick:true,
+                    email:true,
+                    profilePicture:true,
+                    posts:true,
+                    followers:true,
+                    _count: {
+                        select: {
+                            posts:true,
+                            followers:true
+                        },
+                    }
                 }
             })
             if (!user) {
                 throw new AppError(ErrorMessages.USER_NOT_FOUND, StatusCode.STATUS_CODE_CLIENT.NOT_FOUND)
             }
 
-            return user
+            const sanitizedUser: sanitizedOutputSignin = {
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                nick: user.nick,
+                profilePicture: user.profilePicture,
+                followersCount: user._count.followers,  
+                postsCount: user._count.posts,  
+            };
+
+            return sanitizedUser
         } catch (error) {
             throw error
         }
